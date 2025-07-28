89505fc6539eaf05bfdaf7995f3d904dc4a881f6
Your Name <you@example.com>
2025-07-28 19:24:45 +0300

    hw2/1 лишние карточки

---
+++ .history/src/components/AllColumn_20250728185719.vue
@@ -0,0 +1,21 @@
+ <script setup>
+import { ref, onMounted, computed } from 'vue';
+import { getTasks } from './mokcs/tasks.js';
+
+const allTasks = ref([]);
+
+onMounted(() => {
+  allTasks.value = getTasks();
+});
+
+// Создаём вычисляемые свойства для каждой колонки
+const noStatusTask = computed(() => {
+  return allTasks.value.filter((task) => task.status === 'Без статуса');
+});
+
+const doStatusTask = computed(() => {
+  return allTasks.value.filter((task) => task.status === 'Нужно сделать');
+});
+
+// И так далее для каждой колонки...
+</script>
---
+++ .history/src/components/TaskColumn_20250728181359.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main__column column">
+    <div class="column__title">
+      <p>{{ title }}</p>
+    </div>
+    <TaskCard
+      v-for="task in tasks"
+      :key="task.id"
+      :title="task.title"
+      :status="task.status"
+      :topic="task.topic"
+      :date="task.date"
+    />
+  </div>
+</template>
+
+<script setup>
+import TaskCard from './TaskCard.vue'
+
+defineProps({
+  tasks: { type: Array, requireв: true },
+  title: { type: String, require: true },
+})
+</script>
+
+<style lang="scss" scoped></style>
