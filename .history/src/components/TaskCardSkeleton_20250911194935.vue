a70364fd49d3b1657aa22a480b41729412510892
Your Name <you@example.com>
2025-09-11 19:54:13 +0300

    обновленный лоудер

---
+++ .history/src/components/TaskCardSkeleton_20250911194935.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="skeleton">
+    <div class="task-card-skeleton">
+      <div class="skeleton_grup">
+        <div class="skeleton-title"></div>
+        <div class="skeleton-btn"></div>
+      </div>
+      <div class="skeleton-description"></div>
+      <div class="skeleton-deadline"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.task-card-skeleton {
+  width: 220px;
+  height: 130px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: stretch;
+  padding: 15px 13px 19px;
+  margin-bottom: 8px;
+}
+
+.skeleton-title,
+.skeleton-description,
+.skeleton-deadline. 
+.{
+  background-color: #e0e0e0;
+  border-radius: 4px;
+  animation: pulse 1.5s infinite;
+}
+
+.skeleton_grup {
+  display: flex;
+}
+
+.skeleton-title {
+  width: 82px;
+  height: 20px;
+  border-radius: 18px;
+  padding-left: 13px;
+  padding-top: 15px;
+}
+
+.skeleton-btn {
+  width: 18px;
+  height: 4px;
+  margin-left: 185px;
+}
+
+.skeleton-description {
+  width: 113px;
+  height: 13px;
+  padding-left: 13px;
+  margin-top: 15px;
+  margin-bottom: 10px;
+}
+
+.skeleton-deadline {
+  height: 13px;
+  width: 58px;
+  padding-left: 13px;
+  margin-top: 30px;
+}
+
+@keyframes pulse {
+  0% {
+    opacity: 0.5;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.5;
+  }
+}
+</style>
---
+++ .history/src/components/TaskDesk_20250911182851.vue
@@ -0,0 +1,78 @@
+<template>
+  <main class="main">
+    <div class="container">
+      <div class="main__block">
+        <div class="main__content">
+          <TaskColumn
+            v-for="(tasks, status) in statusGroups"
+            :key="status"
+            :tasks="tasks"
+            :title="status"
+            :isLoading="isLoading"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import TaskColumn from './TaskColumn.vue'
+import { ref, onMounted, defineProps, computed } from 'vue'
+
+const props = defineProps({
+  tasks: {
+    type: Array,
+    required: true,
+  },
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+  error: {
+    type: String,
+    default: '',
+  },
+})
+
+const localTasks = ref([])
+
+onMounted(() => {
+  // Имитация загрузки данных
+  setTimeout(() => {
+    localTasks.value = [
+      { id: 1, title: 'Задача 1', description: 'Описание задачи 1', deadline: '2023-12-31' },
+      // Другие задачи...
+    ]
+  }, 2000)
+})
+
+const statusGroups = computed(() => ({
+  'Без статуса': props.tasks.filter((el) => el?.status === 'Без статуса'),
+  'Нужно сделать': props.tasks.filter((el) => el?.status === 'Нужно сделать'),
+  'В работе': props.tasks.filter((el) => el?.status === 'В работе'),
+  Тестирование: props.tasks.filter((el) => el?.status === 'Тестирование'),
+  Готово: props.tasks.filter((el) => el?.status === 'Готово'),
+}))
+</script>
+
+<style lang="scss" scoped>
+.main {
+  width: 100%;
+  background-color: #eaeef6;
+  z-index: 1;
+}
+.main__block {
+  width: 100%;
+  margin: 0 auto;
+  padding: 25px 0 49px;
+}
+.main__content {
+  width: 100%;
+  display: flex;
+}
+
+.taskNo {
+  display: none;
+}
+</style>
